0519803de03f4b589c810bd24b98001085258b17
steve <sdmatthews01@gmail.com>
2024-09-10 22:55:01 -0400

    Added angular material.  changed header to use angular material icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,9 @@ import { HeaderComponent } from "./header/header.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, 
+            HeaderComponent,
+          ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
