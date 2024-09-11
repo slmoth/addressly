806e9a9d95b41232ce77681e1e3ad00ab7720d31
steve <sdmatthews01@gmail.com>
2024-09-10 22:27:49 -0400

    initial commit

---
+++ src/app/login-container/login-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-container.component.html',
+  styleUrl: './login-container.component.scss'
+})
+export class LoginContainerComponent {
+
+}
